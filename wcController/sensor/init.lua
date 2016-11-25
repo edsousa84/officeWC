97f03a78de6a608209ac4268fe41952be8b8aa2d
Rui Esteves <ruicarest@gmail.com>
2016-11-25 20:11:43 +0000

    Read from proximity sensor and send to server.

--- wcController/sensor/init.lua
+++ wcController/sensor/init.lua
@@ -1,31 +1,32 @@
-print(wifi.sta.getip())
-
-
-
 -- ANALOG TEST (PING 
 
-rui = adc.read(0);
-print(rui);
+--rui = adc.read(0);
+--print(rui);
 
 
 -- DIGITAL TEST (PIN D0) 
 
-gpio.mode(1, gpio.INPUT, gpio.PULLUP)
-piu = gpio.read(1)
+--gpio.mode(1, gpio.INPUT, gpio.PULLUP);
+--piu = gpio.read(1);
 
-if piu == 1 then
-   print("pop1");
+gpio.mode(2 , gpio.INPUT );
+proximity = gpio.read(2);
+print("Is someone there");
+print(proximity);
 
-else
-   print("RUI 2")
-end
+
+--if piu == 1 then
+--   print("pop1");
+
+--else
+--   print("RUI 2");
+--end
 
 
 -- MAIN
 
-print(wifi.sta.getip())
-
 wifi.setmode(wifi.STATION)
+--print(wifi.sta.getip())
 
 --MAC = wifi.sta.getmac();
 MAC = "0c:8b:fd:9b:a3:93";
@@ -40,12 +41,13 @@ tmr.alarm(1, 1000, 1, function()
          tmr.stop(1)
          print("Connected, IP is "..wifi.sta.getip())
 --          /device/[type]/[mac]/[interface]/[value]
-            http.get("http://172.20.199.194:8080/device/sensor/" .. MAC .. "/1/" .. rui , nil, function(code, data)
+            http.get("http://172.20.199.194:8080/device/sensor/" .. MAC .. "/1/" .. proximity , nil, function(code, data)
             
             if (code < 0) then
               print("HTTP request failed")
             else
-              print(code, data)
+--            print(code, data)
+              print("RUI is working");
               node.dsleep(5000000)
             end
           end)
